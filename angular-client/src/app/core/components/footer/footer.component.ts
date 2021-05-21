37f789aa5148204783e05f2058a2a064571e2c8c
Viktor Sabov <viktorszabo2002@gmail.com>
2021-05-21 11:18:52 +0300

    fixed layout

--- angular-client/src/app/core/components/footer/footer.component.ts
+++ angular-client/src/app/core/components/footer/footer.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-footer',
     template: `
-    <footer class="footer mt-auto py-3 bg-light">
+    <footer class="footer py-3 bg-light">
         <div class="container">
             <span class="text-muted">
                 Kopirait LPNU 2019-2023.
